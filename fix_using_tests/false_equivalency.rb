5fe4d5000c405e3a85e91172fb0962c0fcf8ca33
Seth Starr <starrseth@hotmail.com>
2020-06-07 18:38:54 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -11,15 +11,12 @@ def prompt_user
   puts "2.) Eat a ham."
 end
 
-def selection(num > 1)
+def selection(num)
   if num = 1 
      "YUM YUM MUNCH MUNCH MUNCH"
   elsif num = 2
      "HAM HAM HAM IN MY TUMMY"
   end 
-  binding.pry
-  puts 'hello'
-
 end
 
 def runner
