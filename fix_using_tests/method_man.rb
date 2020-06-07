6f62ed0553c3387396772e2858851564f68d83e7
Seth Starr <starrseth@hotmail.com>
2020-06-07 19:05:02 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,12 +1,12 @@
 # don't forget to add: require 'pry'
 require 'pry'
 
-$player1 = "Rocky"
-$player2 = "Bullwinkle"
+
 def start_game(player1, player2)
   "Hello #{player1} & #{player2}"
 end
 
+start_game("Rocky", "Bullwinkle")
 
 def play_game(player2)
   "#{player1} is better than #{player2}"
